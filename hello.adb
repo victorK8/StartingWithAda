84b5930571c02b109bf7c0743484e07470a9625c
victorK8 <victormalum@gmail.com>
2020-05-07 17:25:42 +0200

    hello-world with ada

---
+++ hello.adb
@@ -0,0 +1,13 @@
+-- libraries
+with Text_IO; use Text_IO;
+
+-- task
+procedure hello is
+
+-- begin of task
+begin
+	-- sentences
+	Put_Line("Hello World!(starting with ada)");
+
+-- end of task
+end hello;
